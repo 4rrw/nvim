b19508e4e7b566e4ecaab55e47e15404ad411904
Krystian <k.strzalka@numlabs.com>
2026-04-09 18:03:45 +0200

    feat: render codeblock in markdown added jupytext

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -13,7 +13,7 @@ return {
     opts = {
       code = {
         -- Turn on / off code block & inline code rendering
-        enabled = false,
+        enabled = true,
         -- Turn on / off any sign column related rendering
         sign = true,
         -- Determines how code blocks & inline code are rendered:
@@ -70,61 +70,4 @@ return {
       },
     },
   },
-  -- {
-  --   "GCBallesteros/NotebookNavigator.nvim",
-  --   keys = {
-  --     {
-  --       "]h",
-  --       function()
-  --         require("notebook-navigator").move_cell("d")
-  --       end,
-  --     },
-  --     {
-  --       "[h",
-  --       function()
-  --         require("notebook-navigator").move_cell("u")
-  --       end,
-  --     },
-  --     { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-  --     { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-  --   },
-  --   dependencies = {
-  --     "nvim-mini/mini.comment",
-  --     -- "hkupty/iron.nvim", -- repl provider
-  --     -- "akinsho/toggleterm.nvim", -- alternative repl provider
-  --     "benlubas/molten-nvim", -- alternative repl provider
-  --     -- "anuvyklack/hydra.nvim",
-  --   },
-  --   event = "VeryLazy",
-  --   config = function()
-  --     local nn = require("notebook-navigator")
-  --     nn.setup({
-  --       cell_markers = {
-  --         python = "#%%",
-  --       },
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "nvim-mini/mini.hipatterns",
-  --   event = "VeryLazy",
-  --   dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
-  --   opts = function()
-  --     local nn = require("notebook-navigator")
-  --
-  --     local opts = { highlighters = { cells = nn.minihipatterns_spec } }
-  --     return opts
-  --   end,
-  -- },
-  -- {
-  --   "nvim-mini/mini.ai",
-  --   event = "VeryLazy",
-  --   dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
-  --   opts = function()
-  --     local nn = require("notebook-navigator")
-  --
-  --     local opts = { custom_textobjects = { h = nn.miniai_spec } }
-  --     return opts
-  --   end,
-  -- },
 }
